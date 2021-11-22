b777c818b7089e9fe3e3bfe4950b7fac40678012
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-22 21:09:53 +0330

    adding Base Card to the App

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/styleSheet/index.css";
+import BaseCard from "./components/Base/BaseButton.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("base-card", BaseCard)
+  .mount("#app");
