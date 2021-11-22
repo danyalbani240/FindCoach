9a11107ada4d1542726505a4201b9a9437f55362
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-22 21:07:31 +0330

    adding Base Card component

---
+++ src/components/Base/BaseCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card rounded-xl shadow-md p-4 my-8 max-w-screen-sm">
+    <slot> </slot>
+  </div>
+  <!-- <div
+    data-aos="zoom-in-up"
+    data-aos-duration="500"
+    class="bg-secondary rounded-xl pt-16 pb-10 px-10"
+  >
+    <div class="icon">
+      <img class="mx-auto" src="../static/Bitcoin Logo.png" alt="" />
+    </div>
+    <p class="text-Neutral-white text-4xl font-bold text-center mt-10 mb-5">
+      Bitcoin<sup class="text-base mx-2 opacity-70">BTC</sup>
+    </p>
+    <p class="text-Neutral-white text-center">
+      Digital currency in which a record of <br />
+      transactions is maintained.
+    </p>
+    <div class="flex justify-center mt-5">
+      <Button blue="true" name="Start mining" next="true" />
+    </div>
+  </div> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
