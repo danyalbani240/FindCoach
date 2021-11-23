6bf2afee49c68b72410e19f19fd54db32160f676
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-23 10:56:18 +0330

    some changes to the Base Button

--- src/components/Base/BaseButton.vue
+++ src/components/Base/BaseButton.vue
@@ -1,65 +1,13 @@
 <template>
-  <button
-    class="rounded-full inline-flex items-center"
-    :class="{
-      'bg-accent': blue,
-      'bg-Neutral-Green': green,
-      'py-3 px-6': minimal,
-      'py-4 px-8': !minimal,
-      'bg-Neutral-Red': red,
-      'bg-Neutral-white': white,
-      'text-black': tblack,
-      'text-Neutral-white': !tblack,
-    }"
-  >
+  <button class="bg-red-400 p-4 rounded hover:bg-red-500 transition-all text-white">
     {{ name }}
-    <div
-      class="inline-block"
-      :class="{ hidden: !next, 'ml-0': justButton, 'ml-4': !justButton }"
-    >
-      <img src="../assets/next.png" alt="" />
-    </div>
   </button>
 </template>
 <script>
 export default {
-  name: "Button",
   props: {
     name: {
       type: String,
-      required: false,
-    },
-    blue: {
-      type: Boolean,
-      required: false,
-    },
-    next: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    green: {
-      type: Boolean,
-      required: false,
-    },
-    minimal: {
-      type: Boolean,
-      required: false,
-    },
-    red: {
-      type: Boolean,
-      required: false,
-    },
-    white: {
-      type: Boolean,
-      required: false,
-    },
-    tblack: {
-      type: Boolean,
-      required: false,
-    },
-    justButton: {
-      type: Boolean,
       required: false,
     },
   },
