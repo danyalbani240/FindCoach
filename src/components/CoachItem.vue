68734ef4afbfc3fdbabdf67217cfa49d6d1f4264
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-23 10:57:15 +0330

    making coach Item to Card

--- src/components/CoachItem.vue
+++ src/components/CoachItem.vue
@@ -1,21 +1,48 @@
 <template>
   <li>
-    <h3>{{ fullName }}</h3>
-    <h4>{{ rate }}/hour</h4>
-    <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
-    </div>
-    <div class="actions">
-      <button>Contact</button>
-      <router-link :to="coachDetailsLink">ViewDetails</router-link>
-    </div>
+    <base-card>
+      <div
+        data-aos="zoom-in-up"
+        data-aos-duration="700"
+        class="bg-Neutral-white px-7"
+      >
+        <div>
+          <h3 class="text-primary text-4xl font-bold text-center mt-10 mb-5">
+            {{ fullName }}
+          </h3>
+          <h4 class="text-Neutral-Grey3 text-center">
+            {{ `قیمت ${rate} دلار/ساعت` }}
+          </h4>
+          <h4
+            class="text-Neutral-Grey3 text-center"
+            v-for="area in areas"
+            :key="area"
+          >
+            {{ `${area}:شهر ها` }}
+          </h4>
+        </div>
+        <div class="mx-auto next mt-10 flex justify-evenly actions">
+          <base-button name="Contact" />
+
+          <router-link :to="coachDetailsLink">
+            <img
+              class="w-12 cursor-pointer text-center border-2 rounded-full"
+              src="../assets/next.png"
+              alt="next"
+            />
+          </router-link>
+        </div>
+      </div>
+    </base-card>
   </li>
 </template>
 
 <script>
-import { computed, ref } from "@vue/reactivity";
+import { computed } from "@vue/reactivity";
 import { useRoute } from "vue-router";
+import BaseButton from "./Base/BaseButton.vue";
 export default {
+  components: { BaseButton },
   props: {
     firstName: {
       type: String,
