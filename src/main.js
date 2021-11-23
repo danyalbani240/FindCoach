c1df9c4394bfce7beb85ed83c7d987081569b307
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-23 10:56:05 +0330

    adding base Button

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/styleSheet/index.css";
-import BaseCard from "./components/Base/BaseButton.vue";
+import BaseCard from "./components/Base/BaseCard.vue";
+import BaseButton from "./components/Base/BaseButton.vue";
 
 createApp(App)
   .use(store)
   .use(router)
   .component("base-card", BaseCard)
+  .component("base-button", BaseButton)
   .mount("#app");
